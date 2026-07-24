9a007ad4d0a8d90edb80e21da8cb8322ed5911cb
Dheeraj32903 <2400032903@kluniversity.in>
2026-07-24 20:06:09 +0530

    pproject v2.0 modifications in Patient.java and PatientService.java

--- src/main/java/com/klef/soa/service/PatientService.java
+++ src/main/java/com/klef/soa/service/PatientService.java
@@ -1,13 +1,12 @@
 package com.klef.soa.service;
 
-
-
 import java.util.List;
 
 import com.klef.soa.entity.Patient;
 
-public interface PatientService {
-Patient addPatient(Patient patient);
-List<Patient> dsiplayAllPatients();
-Patient updatePatient(Patient p);
+public interface PatientService 
+{
+    Patient addPatient(Patient patient);
+    List<Patient>displayAllPatients();
+    Patient updatePatient(Patient p);
 }
